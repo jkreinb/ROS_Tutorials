97ca9cc31cec41a32d02fc1033adbf6af8a4ff04
Jon <jkreinb@gmail.com>
2021-10-31 21:01:10 -0400

    initial catkin_ws creation

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/jon/Documents/ROS/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/jon/ROS/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/jon/ROS/catkin_ws/devel;/opt/ros/melodic' != '' else []
