ed35e353c00b9c33a819ecb4074cecb3cf4b71a9
Jon <jkreinb@gmail.com>
2021-10-31 21:35:46 -0400

    Completion of BuildingPackages

--- catkin_ws/build/catkin_generated/order_packages.py
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/jon/Documents/ROS/catkin_ws/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/jon/ROS/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/jon/ROS/catkin_ws/devel;/opt/ros/melodic' != '' else []
+underlay_workspaces = '/home/jon/Documents/ROS/catkin_ws/devel;/home/jon/ROS/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/jon/Documents/ROS/catkin_ws/devel;/home/jon/ROS/catkin_ws/devel;/opt/ros/melodic' != '' else []
